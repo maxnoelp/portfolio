518223aa51f59611345d894e7bfa36852522c0eb
Max <noelpmax@gmail.com>
2024-09-30 18:56:50 +0200

    change app.vue to HomeView & navbar new link "guestbook"

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,6 @@
-<script>
-import AboutMe from './components/AboutMe.vue';
-import CodingSkillsComp from './components/CodingSkillsComp.vue';
-import FooterComp from './components/FooterComp.vue';
-import HeaderComp from './components/HeaderComp.vue';
-import NavBarComp from './components/NavBarComp.vue';
-import ProjectComp from './components/ProjectComp.vue';
-
-export default {
-  components: { NavBarComp, HeaderComp, AboutMe, CodingSkillsComp, ProjectComp, FooterComp },
-};
-</script>
+<script></script>
 
 <template>
-  <NavBarComp />
-
-  <header>
-    <HeaderComp />
-  </header>
-  <main>
-    <AboutMe />
-    <CodingSkillsComp />
-    <ProjectComp />
-  </main>
-
-  <footer class="container-fluid m-0 p-0"><FooterComp /></footer>
   <RouterView />
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
+import GuestBookView from '@/view/GuestBookView.vue';
+import HomeView from '@/view/HomeView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
+    {
+      path: '/guestbook',
+      name: 'guestbook',
+      component: GuestBookView,
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
   ],
 });
 
